cf65c78b482c6b09e0eba3176a4ba11a59e0b079
l1lbgdv <hedgehog7421@gmail.com>
2025-09-02 02:06:19 +0700

    khong biet kly co ny chua nhi

--- DE14/ts.cpp
+++ DE14/ts.cpp
@@ -1,26 +1,29 @@
-// author : anphung
+// author : anphung >> npkly
 #include<bits/stdc++.h>
 using namespace std;
 #define int64 long long
 #define f0(i,a,b) for(int (i)=(a);(i)<=(b);++i)
-#define bitmask(mask,i) ((mask>>i)&1)
-int n,t;
+
+int n,m;
+int64 k;
+const int maxn = 1e6+5;
+int64 a[maxn];
 
 int32_t main(){
     ios::sync_with_stdio(0);cin.tie(0);
-    cin>>t;
-    while(t--){
-        cin>>n;
-        if(n==0){
-            cout<<0<<'\n';
-            continue;
-        }
-        int hi = 31 - __builtin_clz(n);
-        for(int i=hi;i>=0;--i){
-            cout<<bitmask(n,i);
-        }
-        cout<<'\n';
+    cin>>m>>n>>k;
+    vector <int64> pre(m+1,0);
+    pre[0] = 0;
+    f0(i,1,m){
+        cin>>a[i];
     }
+    sort(a+1,a+m+1);
+    f0(i,1,m)pre[i] = pre[i-1] + a[i];
+    int64 ans = -2;
+    f0(i,1,m){
+        if(i-n+1>=1&&a[i]-a[i-n+1]<=k)ans = max(ans,pre[i]-pre[i-n]);
+    }
+    cout<<ans;
 }
 
 
--- linh tinh/marisaoj.cpp
+++ linh tinh/marisaoj.cpp
@@ -1,31 +1,34 @@
-// author : anphung
-// github : 4npg
+// author : anphung >> npkly
 #include<bits/stdc++.h>
 using namespace std;
 #define int64 long long
 #define f0(i,a,b) for(int (i)=(a);(i)<=(b);++i)
 
-int n,q;
-const int maxn = 1e5+5;
-
+int n;int64 s;
+vector<int64>w;
+vector<int64>v;
+int64 ans;
+void Try(int i,int64 cw,int64 cv){
+    if(i==n){
+        if(cw<=s)ans = max(ans,cv);
+        return;
+    }
+    Try(i+1,cw,cv);
+    if(cw+w[i]<=s){
+        Try(i+1,cw+w[i],cv+v[i]);
+    }
+}
 
 int32_t main(){
     ios::sync_with_stdio(0);cin.tie(0);
-    cin>>n>>q;
-    vector<int64> pre(n);
-    pre[0] = 0;
-    f0(i,1,n){
-        int64 x;
-        cin>>x;
-        pre[i] = pre[i-1] + x;
+    cin>>n>>s;
+    v.resize(n);w.resize(n);
+    f0(i,0,n-1){
+        cin>>w[i]>>v[i];
     }
-    int64 mx = -1e9;
-    int lo = 1;
-    int hi = n;
-    while(lo<=hi){
-        mx = max(mx,pre[hi]-pre[lo-1]);
-        lo++;
-    }
+    ans=0;
+    Try(0,0,0);
+    cout<<ans;
 }
 
 
