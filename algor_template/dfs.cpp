f4143b4d7a7ac4fcc011e80424e055403fc0e36e
4npg <louisbenjamin3780@gmail.com>
2025-11-28 17:32:05 +0700

    add

--- algor_template/dfs.cpp
+++ algor_template/dfs.cpp
@@ -1,34 +1,49 @@
-#include <bits/stdc++.h>
+// author : anphung
+#include<bits/stdc++.h>
 using namespace std;
+#define int64 long long
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#define f0(i,a,b) for(int (i)=(a);(i)<=(b);++i)
+#define file(name) if(fopen(name".inp","r")){ freopen(name".inp","r",stdin);freopen(name".out","w",stdout);}
+#define maxn 100005
+#define pb push_back
 
-const int MAXN = 200005;
+int n,m;
+int d[maxn];
+bool vis[maxn];
+vector<int> a[maxn];
 
-vector<int> adj[MAXN];
-bool visited[MAXN];
-int n, m;
-
-void dfs(int u) {
-    visited[u] = true;
-    // Xử lý tại đỉnh u
-
-    for (int v : adj[u]) {
-        if (!visited[v]) dfs(v);
+void dfs(int s){
+    vis[s] = 1;
+    for(auto &v:a[s]){
+        if(!vis[v]){
+            d[v] = d[s] + 1;
+            dfs(v);
+        }
     }
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> n >> m;
-    while (m--) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+int32_t main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    cin>>n>>m;
+    f0(i,0,m-1){
+        int u,v;
+        cin>>u>>v;
+        a[u].pb(v);
+        a[v].pb(u);
     }
+    memset(d, -1, sizeof(d));
+    d[1] = 0;
 
     dfs(1);
-
-    return 0;
+    f0(i,2,n){
+        cout<<d[i]<<" ";
+    }
+    cerr << "time elapsed: "<<TIME <<"s.\n";
 }
+
+
+
+
+
