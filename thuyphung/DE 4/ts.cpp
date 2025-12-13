f39686b89631ddebbf66b1e85a416a572e507e9a
4npg <louisbenjamin3780@gmail.com>
2025-12-13 16:36:23 +0700

    add khao sat 13/12/2025

--- thuyphung/DE 4/ts.cpp
+++ thuyphung/DE 4/ts.cpp
@@ -4,31 +4,45 @@ using namespace std;
 #define int64 long long
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 #define f0(i,a,b) for(int (i)=(a);(i)<=(b);++i)
-#define file(name) freopen(name".inp","r",stdin);freopen(name".out","w",stdout);
-#define maxn 100005
-
-int pre[maxn];
-int ans = INT_MIN;
+#define file "WOOD"
 int n;
+int64 s;
+
+mt19937 rd(time(0));
+int Rand(int l, int r) {
+    return l + rd() % (r - l + 1);
+}
 
 int32_t main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // ofstream cout(file".inp");
+    // n = Rand(1,1000006);
+    // s = Rand(1,2*100000009);
+    //cout<<n<<" "<<s<<'\n';
     
+    cin>>n>>s;
+    vector<int64> a(n);
 
-    cin>>n;
-    pre[0] = 0;
-    f0(i, 1, n){
-        int x; cin>>x;
-        pre[i] = pre[i-1] + x;
+    f0(i,0,n-1){
+        // a[i] = Rand(1,1000000009);
+        // cout << a[i]<<" ";
+        cin>>a[i];
+    }
+    int64 sum = 0;
+    int lo = 0;
+    int res = 1e9;
+
+    f0(hi,0,n-1){
+        sum+=a[hi];
+        while(sum>=s){
+            res = min(res,hi-lo+1);
+            sum-=a[lo];
+            lo++;
+        }
     }
 
-    f0(i, 1, n){
-        int s1 = ((pre[i])*(-1) + (pre[n]-pre[i]));
-        int s2 = (pre[i] + (pre[n]-pre[i])*(-1));
-        ans = max(ans, max(s1, s2));
-    }
+    cout<<"\nres = "<<res;
 
-    cout<<ans;
     cerr << "time elapsed: "<<TIME <<"s.\n";
 }
 
