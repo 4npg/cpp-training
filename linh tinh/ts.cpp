730a3689dddcf00cd2c5207e08aa2e503f3551b9
4npg <louisbenjamin3780@gmail.com>
2025-12-17 07:04:30 +0700

    add r

--- linh tinh/ts.cpp
+++ linh tinh/ts.cpp
@@ -9,13 +9,53 @@ using namespace std;
 
 //mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
 
+// void Rand(int l, int r){
+//     return l+rng()%(r-l+1);
+// }
+
 #define maxn 
 
 i_love_Hoang_Ngan(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
+    int N,M;
+    cin>>N>>M;
 
+    vector<vector<int>> g(N+1);
+    f0(i,1,M){
+        int u,v;
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    vector<char> vis(N+1,0);
+    int C=0;
+
+    f0(i,1,N){
+        if(!vis[i]){
+            C++;
+            stack<int> st;
+            st.push(i);
+            vis[i]=1;
+            while(!st.empty()){
+                int u=st.top(); st.pop();
+                for(int v:g[u]){
+                    if(!vis[v]){
+                        vis[v]=1;
+                        st.push(v);
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<M-N+C;
+    
     cerr << "time elapsed: "<<TIME <<"s.\n";
 }
+
+
+
 
 
