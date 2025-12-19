02482e4a354ef93001a24b6e4356b4b9029f096e
l1lbgdv <hedgehog7421@gmail.com>
2025-12-20 00:33:25 +0700

    no

--- linh tinh/marisaoj.cpp
+++ linh tinh/marisaoj.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define i_love_Hoang_Ngan int32_t main
-#define int64 long long
+#define int64 unsigned long long
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 #define f0(i,a,b) for(int (i)=(a);(i)<=(b);++i)
 #define fd(i,a,b) for(int (i)=(a);(i)>=(b);--i)
@@ -14,53 +14,47 @@ using namespace std;
 //     return l+rng()%(r-l+1);
 // }
 
-#define maxn 200005
-#define pli pair<int64, int>
-#define inf (int64)4e18
-#define fi first 
-#define se second 
+#define maxn (int64)1e18
 #define pb push_back
 
-int n, m;
-vector<pli> a[maxn];
-int64 d[maxn];
+vector<int64> a;
+int q;
 
-i_love_Hoang_Ngan(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void sinh(){
+    for(int i=0;i<=60;i++){
+        int64 p2 = 1;
+        for(int x=0;x<i;x++) p2 *= 2;
 
+        for(int j=0;j<=38;j++){
+            int64 p23 = p2;
+            for(int x=0;x<j;x++) p23 *= 3;
 
-    cin>>n>>m;
+            for(int k=0;k<=26;k++){
+                int64 val = p23;
+                for(int x=0;x<k;x++) val *= 5;
 
-    f0(i, 1, n)d[i] = inf;
-    d[1] = 0;
-
-    f0(i, 0, m-1){
-        int u, v, w;
-        cin>>u>>v>>w;
-        a[u].pb({v, w});
-        a[v].pb({u, w});
-    }
-
-    priority_queue<pli> q;
-    q.push({-d[1], 1});
-
-    while(q.size()){
-        int64 du = -q.top().fi;
-        int u = q.top().se;
-        q.pop();
-        if(du != d[u])continue;
-        for(auto &p:a[u]){
-            int v = p.fi;
-            int64 w = p.se;
-
-            if(d[u] + w < d[v]){
-                d[v] = d[u] + w;
-                q.push({-d[v], v});
+                if(val <= 1e18*1ULL)
+                    a.pb(val);
             }
         }
     }
+}
+i_love_Hoang_Ngan(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    sinh();
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()),a.end());
+    cin>>q;
+    while(q--){
+        int64 m;
+        cin>>m;
 
-    cout<<d[n];
+        auto pos = lower_bound(a.begin(), a.end(), m);
+        if(pos != a.end() && *pos == m){
+            cout<<(pos-a.begin()+1)<<'\n';
+        }else cout<<-1<<'\n';
+    }
+
     cerr << "time elapsed: "<<TIME <<"s.\n";
 }
 
