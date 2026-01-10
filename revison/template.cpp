92cfa70ffcd8eb3a3f770e48f3fdbf2bc1a07bc2
4npg <louisbenjamin3780@gmail.com>
2026-01-10 13:47:50 +0700

    thi xong roi

--- revison/template.cpp
+++ revison/template.cpp
@@ -10,18 +10,24 @@ using namespace std;
 //mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
 
 //long long Rand(long long l, long long r){
-//    return l+rng()%(r-l+1);
+//	return l+rng()%(r-l+1);
 //}
 
-#define maxn 
+#define maxn 310
 #define lg 20
 #define inf (long long)4e18
 #define mod (long long)(1e9+7)
 
-con_meo_dua_leo(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    // freopen(file".inp", "r", stdin);
-    // freopen(file".out", "w", stdout);
+int M, U, R;
 
-    cerr << "\ntime elapsed: "<<TIME <<"s.\n";
+int V[maxn], T[maxn], F[maxn];
+
+int dp[maxn][maxn];
+
+con_meo_dua_leo(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// freopen(file".inp", "r", stdin);
+	// freopen(file".out", "w", stdout);
+
+	cerr << "\ntime elapsed: "<<TIME <<"s.\n";
 }
