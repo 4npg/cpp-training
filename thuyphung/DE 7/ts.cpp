2f2842e0e7922c97a8f1434afe21aba65d5b3436
4npg <louisbenjamin3780@gmail.com>
2026-01-12 01:05:11 +0700

    end

--- thuyphung/DE 7/ts.cpp
+++ thuyphung/DE 7/ts.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 #define f0(i, a, b) for(int i = (a); i <=(b); ++i)
 #define fd(i, a, b) for(int i = (a); i >=(b); --i)
-#define file ""
+#define file "cau1"
 
 //mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
 
@@ -18,10 +18,37 @@ using namespace std;
 #define inf (long long)4e18
 #define mod (long long)(1e9+7)
 
+int a[8];
+bool ok1 = true;
+bool ok2 = true;
+
 con_meo_dua_leo(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    // freopen(file".inp", "r", stdin);
-    // freopen(file".out", "w", stdout);
+     freopen(file".inp", "r", stdin);
+     freopen(file".out", "w", stdout);
 
+    f0(i, 0, 7)cin>>a[i];
+
+    f0(i, 1, 6){
+        if(a[i-1] < a[i] && a[i] < a[i+1]){
+            ok2 = false;
+        }
+
+        if(a[i-1] > a[i] && a[i] > a[i+1]){
+            ok1 = false;
+        }
+    }
+
+    if(ok1 == true && ok2 == false){
+        return cout<<"ascending", 0;
+    }
+
+    if(ok1 == false && ok2 == true){
+        return cout<<"decending", 0;
+    }
+
+    if(ok1 == false && ok2 == false){
+        return cout<<"mixed", 0;
+    }
     cerr << "\ntime elapsed: "<<TIME <<"s.\n";
 }
