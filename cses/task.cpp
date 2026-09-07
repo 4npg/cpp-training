581e55ee09563e7e06c0ad54747148508fb761d7
l1lbgdv <hedgehog7421@gmail.com>
2026-09-08 00:06:57 +0700

    add

--- cses/task.cpp
+++ cses/task.cpp
@@ -24,8 +24,65 @@ long long rl(long long l, long long r) {
     return uniform_int_distribution<long long>(l, r)(rd);
 }
 
-void solve() {
+int t;
 
+void solve() {
+    cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int> a(n);
+        for (int &x : a) cin >> x;
+
+        vector<int> pos;
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0) pos.push_back(i);
+        }
+
+        if (pos.empty()) {
+            for (int i = 0; i < n; i++) cout << 0 << ' ';
+            cout << '\n';
+            continue;
+        }
+
+        int bestL = pos[0], bestR = pos[0];
+        int maxDist = 1;
+
+        for (int i = 0; i + 1 < (int)pos.size(); i++) {
+            int l = pos[i];
+            int r = pos[i + 1];
+
+            bool hasFixedOne = false;
+            for (int k = l + 1; k < r; k++) {
+                if (a[k] == 1) {
+                    hasFixedOne = true;
+                    break;
+                }
+            }
+
+            if (!hasFixedOne) {
+                int dist = r - l + 1;
+                if (dist > maxDist) {
+                    maxDist = dist;
+                    bestL = l;
+                    bestR = r;
+                }
+            }
+        }
+
+        vector<int> ans = a;
+        ans[bestL] = 1;
+        ans[bestR] = 1;
+
+        for (int i = bestL + 1; i < bestR; i++) {
+            if (ans[i] == -1) ans[i] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (ans[i] == -1) ans[i] = 0;
+            cout << ans[i] << ' ';
+        }
+        cout << '\n';
+    }
 }
 
 int main() {
